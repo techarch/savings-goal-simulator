6b7dc6edade5c61ed566d6504a77deea3c4213fd
unknown <techarch@monnet-usa.com>
2011-10-16 17:36:19 -0600

    Initial code for todo #6 of part 2

--- scripts/application.js
+++ scripts/application.js
@@ -1,4 +1,22 @@
+// Lazy initialize our namespaces
+if (typeof(sgs) == 'undefined') sgs = { }
+if (typeof(sgs.model) == 'undefined') sgs.model = { }
+if (typeof(sgs.mediator) == 'undefined') sgs.mediator = { }
+
+if (typeof(console) != 'undefined' && console) console.info("Application loading!");
+
 function InitializeApplication() {
 	if (typeof(console) != 'undefined' && console) 
 		console.info("InitializeApplication starting ...");
+	
+	// Initialize our page-wide settings
+	var pageSettings = { defaultSavingsGoal: 500 }
+	
+	// Create / launch our view mediator(s)
+	sgs.mediator.savingsgoal.createViewMediator(pageSettings);
+
+	if (typeof(console) != 'undefined' && console) 
+		console.info("InitializeApplication done ...");	
 }
+
+if (typeof(console) != 'undefined' && console) console.info("Application loaded!");
