91034f2230135a3856ae53a848c8e40f6f754341
unknown <techarch@monnet-usa.com>
2012-01-07 15:18:17 -0700

    Initial code for  todo #6 of part 4

--- scripts/viewmediator/sgs.mediator.consumption-scenarios.js
+++ scripts/viewmediator/sgs.mediator.consumption-scenarios.js
@@ -9,6 +9,21 @@ sgs.mediator.consumptionscenarios.createViewMediator = function (pageSettings) {
 	// Create the view Consumption Scenarios view-specific view model
 	var viewModel = sgs.model.consumptionscenarios.initializeViewModel(pageSettings);
 	
+	// Save the view model
+	sgs.mediator.consumptionscenarios.setViewModel(viewModel);	
+
+	// Set the pricing based on the Coffee Pricing view model
+	var priceList = sgs.mediator.coffeepricing.getViewModel();
+	viewModel.pricing(priceList);
+	
+	// Ask KnockoutJS to data-bind the view model to the view
+	var viewNode = $('#consumption-scenarios-view-container')[0];
+	ko.applyBindings(viewModel, viewNode);
+	
+	if (typeof(console) != 'undefined' && console) console.info("sgs.mediator.coffeeconsumption ready!");
+}
+
+sgs.mediator.consumptionscenarios.setupViewDataBindings = function(x, y) {
 	// Declare the HTML element-level data bindings for the Current Habits column
 	$("#current-drink-type  input[type=radio]")
 		.attr("data-bind","checked: currentConsumption().drinkType");
@@ -40,24 +55,19 @@ sgs.mediator.consumptionscenarios.createViewMediator = function (pageSettings) {
 		.attr("data-bind","text: proposedConsumption().costPerWeekFormatted");
 	$("#savings-per-week")
 		.attr("data-bind","text: savingsPerWeekFormatted");
-	
-	// Ask KnockoutJS to data-bind the view model to the view
+
+		// Ask KnockoutJS to data-bind the view model to the view
 	var viewNode = $('#consumption-scenarios-view')[0];
+	var viewModel = sgs.mediator.consumptionscenarios.getViewModel();
 	ko.applyBindings(viewModel, viewNode);
-
-	// Save the view model
-	sgs.mediator.consumptionscenarios.setViewModel(viewModel);	
-
-	// Set the pricing based on the Coffee Pricing view model
-	var priceList = sgs.mediator.coffeepricing.getViewModel();
-	viewModel.pricing(priceList);
 	
 	viewModel.savingsPerWeekFormatted.subscribe(function() {
 		$("#savings-per-week")
 			.effect('highlight', { color: 'LightGreen' }, 2000); // for 2 seconds
 	});
-	
-	if (typeof(console) != 'undefined' && console) console.info("sgs.mediator.coffeeconsumption ready!");
+
+	if (typeof(console) != 'undefined' && console) 
+		console.info("sgs.mediator.consumptionscenarios.setupViewDataBindings done!");
 }
 
 sgs.mediator.consumptionscenarios.getViewModel = function() {
